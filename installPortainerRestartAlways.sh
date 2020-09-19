5ed25de1bab1f4febd2076dc4d7721c5420849d7
yannick sudrie <yannick.sudrie@gmail.com>
2020-09-20 00:36:03 +0200

    Update and rename installPortnairRestartAlways.sh to installPortainerRestartAlways.sh

--- installPortainerRestartAlways.sh
+++ installPortainerRestartAlways.sh
@@ -2,10 +2,10 @@
 
 #original code from https://portainer.io/install.html
 
-#Create a volume data for portnair
-docker volume create portnair_data
+#Create a volume data for portainer
+docker volume create portainer_data
 
 #install portnair withe auomatic restart at reboot reboot 
 docker run -d --restart=always -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
-echo "In order to acces to portnair interface use your web broser on http://@ipportnair:9000 and choose option localport !"
+echo "In order to acces to portainer interface use your web broser on http://@ipportnair:9000 and choose option localport !"
