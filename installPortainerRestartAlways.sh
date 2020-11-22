7d2ac4b3a1cb1dd3deb0210e3169072bf2f2f45d
yannick sudrie <yannick.sudrie@gmail.com>
2020-11-22 19:50:22 +0100

    Update installPortainerRestartAlways.sh
    switch from portainer/portainer to portainer/portainer-ce

--- installPortainerRestartAlways.sh
+++ installPortainerRestartAlways.sh
@@ -6,6 +6,6 @@
 docker volume create portainer_data
 
 #install portnair withe auomatic restart at reboot reboot 
-docker run -d --restart=always -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d --restart=always -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 echo "In order to acces to portainer interface use your web broser on http://@ipportnair:9000 and choose option localport !"
